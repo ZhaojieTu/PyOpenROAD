c56593ee939ef6133bc19585f0658bc94538d316
Zhaojie Tu <tuzj@mail.ustc.edu.cn>
2025-04-06 15:32:26 +0000

    Update BuildPythonPackage.sh

--- etc/BuildPythonPackage.sh
+++ etc/BuildPythonPackage.sh
@@ -8,3 +8,5 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /opt/conda
 source /opt/conda/bin/activate
 popd
+source /opt/conda/bin/activate
+conda deactivate
